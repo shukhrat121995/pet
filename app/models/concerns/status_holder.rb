bbe8c137838f16d88b7a3233ff6da6288190c902
unknown <shux121995@gmail.com>
2021-12-17 13:23:27 +0200

    creates status holder

---
+++ app/models/concerns/status_holder.rb
@@ -0,0 +1,50 @@
+# Adds methods for managing status column.
+# Add hold_status macros.
+# Example:
+# hold_status :draft, :published, :removed
+module StatusHolder
+  extend ActiveSupport::Concern
+  
+  included do
+    class_attribute :_default_status, :_statuses, instance_writer: false
+    self._statuses ||= []
+  
+    after_initialize { self.status ||= _default_status }
+    validates :status, presence: true, inclusion: {in: ->(r) { r.class._statuses }}
+  
+    scope :with_status, ->(*statuses) { where(status: statuses.flatten.map(&:to_s)) }
+    scope :except_status, ->(*statuses) { where.not(status: statuses.flatten.map(&:to_s)) }
+  end
+  
+  def status
+    read_attribute(:status).try(:to_sym)
+  end
+  
+  def status=(value)
+    value = value.try(:to_s)
+    status_will_change! if value != status.to_s
+    write_attribute(:status, value)
+  end
+  
+  module ClassMethods
+    # Add the status to the list of available status
+    def hold_status(*names)
+      names.flatten.map(&:to_sym).each do |status_name|
+        self._default_status ||= status_name if _statuses.empty?
+        self._statuses = _statuses | [status_name]
+  
+        unless singleton_class.method_defined? status_name
+          define_singleton_method status_name do
+            with_status(status_name)
+          end
+        end
+  
+        next if method_defined? "#{status_name}?"
+  
+        define_method "#{status_name}?" do
+          status == status_name
+        end
+      end
+    end
+  end
+end
---
+++ spec/models/concerns/status_holder_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe StatusHolder do
+  subject(:instance) { model_class.new }
+
+  let(:model_class) do
+    stub_model_class('DummyModel') do
+      self.table_name = 'landing_pages'
+      include StatusHolder
+
+      hold_status :draft, :active, :removed
+    end
+  end
+
+  def create_dummy(status:)
+    model_class.create!(status: status, category: 'freelance', slug: FFaker::Internet.unique.slug.parameterize)
+  end
+
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to validate_inclusion_of(:status).in_array(%i[draft active removed]) }
+  its(:status) { is_expected.to eq(:draft) }
+
+  it { is_expected.to be_draft }
+  it { is_expected.not_to be_active }
+  it { is_expected.not_to be_removed }
+
+  describe 'scopes' do
+    let!(:draft_model) { create_dummy(status: :draft) }
+    let!(:active_model) { create_dummy(status: :active) }
+    let!(:removed_model) { create_dummy(status: :removed) }
+
+    describe '.with_status' do
+      specify do
+        expect(model_class.with_status(:draft)).to match_array([draft_model])
+        expect(model_class.with_status(:active, :removed)).to match_array([active_model, removed_model])
+        expect(model_class.with_status).to match_array([])
+      end
+    end
+
+    describe '.except_status' do
+      specify do
+        expect(model_class.except_status(:draft)).to match_array([active_model, removed_model])
+        expect(model_class.except_status(:active, :removed)).to match_array([draft_model])
+        expect(model_class.except_status).to match_array([draft_model, active_model, removed_model])
+      end
+    end
+  end
+end
