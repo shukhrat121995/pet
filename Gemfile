6f324ac7779d0b15f230a6f43433c930c39af7ee
unknown <shux121995@gmail.com>
2021-12-23 11:16:27 +0200

    installs rubocop

--- Gemfile
+++ Gemfile
@@ -29,11 +29,12 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 5.0.0'
-  gem 'factory_bot'
+  gem 'factory_bot_rails'
   gem 'rails-controller-testing'
 end
 
 group :development do
+  gem 'rubocop', require: false
 end
 
 group :test do
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:product) { build :product }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:status) }
+    it { is_expected.to validate_presence_of(:image) }
+  end
 end
+
