14c2743ec2cdd150bd85343c391be82b7f025bf9
unknown <shux121995@gmail.com>
2021-12-28 13:42:01 +0200

    seeds database with predefined values

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+
+Product.create(
+  name: 'MacBook Pro 14\'',
+  price: 1999.99,
+  description: '14 inch MacBook Pro, 512GB, 16GB RAM',
+  status: 'active',
+  image: 'https://d1fmx1rbmqrxrr.cloudfront.net/cnet/i/edit/2021/10/apple-macbook-pro-14-2021-1200.jpg'
+)
+
+puts "Seeding done."
--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :product do
     name { 'MacBook Pro 14\'' }
-    price { '2000' }
+    price { 1999.99 }
     description { '14 inch MacBook Pro, 512GB, 16GB RAM' }
     status { 'active' }
     image { 'https://d1fmx1rbmqrxrr.cloudfront.net/cnet/i/edit/2021/10/apple-macbook-pro-14-2021-1200.jpg' }
