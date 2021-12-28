f37a674718437f445d11e10403d3eb25fa233737
Shukhrat <41660866+shukhrat121995@users.noreply.github.com>
2021-12-28 13:46:15 +0200

    seeds database with predefined values (#8)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding...'
+
+Product.create(
+  name: 'MacBook Pro 14\'',
+  price: 1999.99,
+  description: '14 inch MacBook Pro, 512GB, 16GB RAM',
+  status: 'active',
+  image: 'https://d1fmx1rbmqrxrr.cloudfront.net/cnet/i/edit/2021/10/apple-macbook-pro-14-2021-1200.jpg'
+)
+
+puts 'Seeding done.'
