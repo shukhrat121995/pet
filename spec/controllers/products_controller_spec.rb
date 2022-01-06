a85e71ac6a9eefe5883a61624ded0ce3538c704a
unknown <shux121995@gmail.com>
2022-01-06 16:18:52 +0200

    Adds unit test that verifies response content type

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,9 +4,15 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   describe 'GET index' do
+    subject(:product) { response }    
     it 'has a 200 status code' do
       get :index
       expect(response.status).to eq(200)
+    end
+    it 'responds with a valid content type' do
+      get :index
+      puts response.content_type.to_yaml
+      expect(response.content_type).to include('application/json')
     end
   end
 end
